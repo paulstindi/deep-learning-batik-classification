5e306ad5c8f209c0945726823f321ef2d51e0e57
Yohanes Gultom <yohanes.gultom@gmail.com>
2016-11-07 22:00:52 +0700

    data augmentation; handle data bigger than ram

--- preprocess.py
+++ preprocess.py
@@ -9,6 +9,67 @@ import cv2
 import numpy as np
 import json
 import progressbar
+import pylab
+import tables
+import scipy.ndimage.interpolation
+import os.path as path
+
+EXPECTED_SIZE = 224
+EXPECTED_CHANNELS = 3
+EXPECTED_DIM = (EXPECTED_CHANNELS, EXPECTED_SIZE, EXPECTED_SIZE)
+EXPECTED_CLASS = 5
+MAX_VALUE = 255
+
+
+def square_slice_generator(data, size, slices_per_axis=5):
+    if data.shape[0] <= size or data.shape[1] <= size:
+        # scale up
+        scale_row = 1.0 * size / data.shape[0]
+        scale_col = 1.0 * size / data.shape[1]
+        resized = scipy.ndimage.interpolation.zoom(data, (scale_row, scale_col, 1), order=3, prefilter=True)
+        resized = resized * 1.0 / MAX_VALUE
+        # print(resized.shape)
+        # plot_comparison((data, resized))
+        yield(resized)
+    else:
+        remaining_rows = data.shape[0] - size
+        remaining_cols = data.shape[1] - size
+        slide_delta_rows = remaining_rows / slices_per_axis
+        slide_delta_cols = remaining_cols / slices_per_axis
+        # print(data.shape)
+        for i in range(slices_per_axis):
+            row_start = i + i * slide_delta_rows
+            row_end = row_start + size
+            for j in range(slices_per_axis):
+                col_start = j + j * slide_delta_cols
+                col_end = col_start + size
+                tmp = data[row_start:row_end, col_start:col_end, :]
+                tmp = tmp * 1.0 / MAX_VALUE
+                yield(tmp)
+                # print('({}:{}, {}:{})'.format(row_start, row_end, col_start, col_end))
+                # plot_comparison((data, tmp))
+
+
+def center_crop(data):
+    # crop
+    median_x = data.shape[0] / 2
+    median_y = data.shape[1] / 2
+    delta_x = data.shape[0] / 3
+    delta_y = data.shape[1] / 3
+    cropped = data[median_x - delta_x:median_x + delta_x, median_y - delta_y:median_y + delta_y]
+    # render image
+    # plot_comparison((data, cropped))
+    return cropped
+
+
+def plot_comparison(images):
+    fig = pylab.figure()
+    for i, m in enumerate(images):
+        fig.add_subplot(len(images), 1, i + 1)
+        pylab.imshow(m)
+    pylab.show()
+    cv2.waitKey(0)
+
 
 # get absolute path from arg
 mypath = sys.argv[1]
@@ -16,45 +77,50 @@ dataset_file = sys.argv[2]
 index_file = sys.argv[3]
 
 # iterate dir content
+stat = {}
 label_indexes = {}
-labels = []
-data = []
-bar = progressbar.ProgressBar(max_value=1000)
+bar = progressbar.ProgressBar(maxval=700).start()
 count = 1
 i = 0
+
+# pytables file
+datafile = tables.open_file(dataset_file, mode='w')
+data = datafile.create_earray(datafile.root, 'data', tables.Float32Atom(shape=EXPECTED_DIM), (0,), 'batik')
+labels = datafile.create_earray(datafile.root, 'labels', tables.UInt8Atom(shape=(EXPECTED_CLASS)), (0,), 'batik')
+
+# iterate subfolders
 for f in os.listdir(mypath):
     path = os.path.join(mypath, f)
+    # exclude Mix motif
     if os.path.isdir(path) and f != 'Mix motif':
         label_indexes[i] = f
         for f_sub in os.listdir(path):
             path_sub = os.path.join(path, f_sub)
             if os.path.isfile(path_sub):
-                im = cv2.resize(cv2.imread(path_sub), (224, 224)).astype(np.float32)
-                # TODO calculate properly
-                im[:, :, 0] -= 103.939
-                im[:, :, 1] -= 116.779
-                im[:, :, 2] -= 123.68
-                im = im.transpose((2, 0, 1))
-                data.append(im)
-                label = [0, 0, 0, 0, 0]
-                label[i] = 1
-                labels.append(label)
+                im = cv2.imread(path_sub)
+                # gather stat
+                if im.shape not in stat:
+                    stat[im.shape] = 0
+                stat[im.shape] += 1
+                for square in square_slice_generator(im, EXPECTED_SIZE):
+                    # transpose to match VGG-16 input
+                    square = square.transpose((2, 0, 1))
+                    data.append(np.array([square]))
+                    # one-hot label
+                    label = np.zeros(5)
+                    label[i] = 1.0
+                    labels.append(np.array([label]))
+                # update progress bar
                 bar.update(count)
-                count = count + 1
-        i = i + 1
+                count += 1
+        i += 1
 bar.finish()
 
-# convert to numpy
-x = np.array(data)
-y = np.array(labels)
-
-# write dataset to file
-np.savez(dataset_file, x=x, y=y)
+print('{} records saved'.format(data.nrows))
 
 # write label index as json file
 with open(index_file, 'w') as f:
     json.dump(label_indexes, f)
 
-# print(label_indexes)
-# print(x.shape)
-# print(y.shape)
+# close file
+datafile.close()
--- train.py
+++ train.py
@@ -9,17 +9,21 @@ import numpy as np
 import sys
 import cv2
 import json
+import tables
+import math
 from keras.models import Sequential
 from keras.layers.core import Flatten, Dense, Dropout
 from keras.layers.convolutional import Convolution2D, MaxPooling2D, ZeroPadding2D
 from keras.optimizers import SGD
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 from datetime import datetime
 
 # training parameters
-BATCH_SIZE = 10
+BATCH_SIZE = 50
 NB_EPOCH = 5
 
+# dataset
+DATASET_BATCH_SIZE = 500
 
 def VGG_16(weights_path=None, input_shape=(3, 224, 224), nb_class=5):
     model = Sequential()
@@ -87,30 +91,38 @@ with open(dataset_index_file, 'r') as f:
 
 # loading dataset
 print('Loading dataset')
-npzfile = np.load(dataset_file)
-X = npzfile['x']
-Y = npzfile['y']
-
-# print(dataset_index)
-# print(len(dataset_index))
-# print(X.shape)
-# print(Y.shape)
-
-print('Spliting dataset')
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.10)
+# npzfile = np.load(dataset_file)
+# X = npzfile['x']
+# Y = npzfile['y']
+datafile = tables.open_file(dataset_file, mode='r').root
 
 # setup model
 print('Preparing model')
-model = VGG_16('../vgg16_weights.h5', X[0].shape, len(dataset_index))
+model = VGG_16('../vgg16_weights.h5', datafile.data[0].shape, len(dataset_index))
+
 
 # training model
-print('Training model')
-model.fit(X_train, Y_train,
-          batch_size=BATCH_SIZE,
-          nb_epoch=NB_EPOCH,
-          validation_data=(X_test, Y_test),
-          shuffle=True)
+num_rows = datafile.data.nrows
+num_iterate = int(math.ceil(num_rows * 1.0 / DATASET_BATCH_SIZE))
+print('Training model using {} data in batch of {}'.format(num_rows, DATASET_BATCH_SIZE))
+for e in range(NB_EPOCH):
+    for i in range(num_iterate):
+        print('Data batch {}/{}'.format(i, num_iterate))
+        begin = i + i * DATASET_BATCH_SIZE
+        end = begin + DATASET_BATCH_SIZE
+        X_train, X_test, Y_train, Y_test = train_test_split(
+            datafile.data[begin:end], datafile.labels[begin:end],
+            test_size=0.10
+        )
+        model.fit(X_train, Y_train,
+                  batch_size=BATCH_SIZE,
+                  nb_epoch=1,
+                  validation_data=(X_test, Y_test),
+                  shuffle=True)
 
 # saving model
 print('Saving model')
 model.save('model_{}.h5'.format(datetime.now().strftime('%Y%m%d%H%M%S')))
+
+# close dataset
+datafile.close()
